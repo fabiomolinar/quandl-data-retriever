84ff34f28e87c3048d1fc84d46ebcafde2f48fd5
fabiomolinar <fabiomolinar@gmail.com>
2019-02-16 22:07:50 +0100

    first quandl api test

--- quandl_data_retriever/server.py
+++ quandl_data_retriever/server.py
@@ -1,2 +1,10 @@
+import settings
+import quandl
+import pandas as pd
+import numpy as np
+
 def main():
-    print("Hi again")
+    api_key = settings.QUANDL_API_KEY
+    quandl.ApiConfig.api_key = api_key
+    data = quandl.get("EIA/PET_RWTC_D")
+    print(data)
---
+++ quandl_data_retriever/settings.py
@@ -0,0 +1,3 @@
+import os
+
+QUANDL_API_KEY = os.environ.get('QUANDL_KEY', '')
