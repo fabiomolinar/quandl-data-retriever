2a30386591e431cb55a0a766b5d0ce102868db84
fabiomolinar <fabiomolinar@gmail.com>
2019-02-17 01:59:14 +0100

    changing to twisted

--- quandl_data_retriever/server.py
+++ quandl_data_retriever/server.py
@@ -4,13 +4,37 @@ Quandl API limits:
 Authenticated users have a limit of 300 calls per 10 seconds, 
 2,000 calls per 10 minutes and a limit of 50,000 calls per day.
 """
+import urllib
+from twisted.internet import reactor
+from twisted.web.client import Agent
+
 import settings
-import quandl
-import asyncio
-import pandas as pd
-import numpy as np
 
 API_KEY = settings.QUANDL_API_KEY
 
-async def main():
-    print("My first async def")
+def main():
+    uri = urllib.parse.urlunparse((
+        settings.QUANDL_API_ENDPOINT["scheme"],
+        settings.QUANDL_API_ENDPOINT["netloc"],
+        settings.QUANDL_API_ENDPOINT["path"] + 
+            settings.QUANDL_API_DATATYPE["timeseries"] + 
+            "WIKI/FB/data.json",
+        "",
+        "api_key=" + API_KEY,
+        ""
+    ))
+    agent = Agent(reactor)
+    d = agent.request(
+        "GET",
+        str.encode(uri)
+    )
+    
+    def cbResponse(response):
+        print("called")
+    d.addCallback(cbResponse)
+    
+    def cbShutdown(ignored):
+        reactor.stop()
+    d.addBoth(cbShutdown)
+
+    reactor.run()
--- quandl_data_retriever/settings.py
+++ quandl_data_retriever/settings.py
@@ -1,6 +1,15 @@
 import os
 
-QUANDL_API_KEY = os.environ.get('QUANDL_KEY', '')
-DEBUG = os.environ.get('DEBUG_MODE', 'False') == 'True'
+QUANDL_API_KEY = os.environ.get("QUANDL_KEY", "")
+QUANDL_API_ENDPOINT = {
+    "scheme": "https",
+    "netloc": "www.quandl.com",
+    "path": "/api/v3/"
+}
+QUANDL_API_DATATYPE = {
+    "timeseries": "datasets/",
+    "tables": "datatables/"
+}
+DEBUG = os.environ.get("DEBUG_MODE", "False") == "True"
 # Every how many seconds should we ask for data
 FREQUENCY = 10 * 60
